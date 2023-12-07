9e68ee10235fdb7013e9b61ea7dfbdccb9952331
Karisma-471 <okoyecharisma@gmail.com>
2023-12-07 08:31:56 +0100

    a program that generates a random number

---
+++ Rand_Num/randNum.js
@@ -0,0 +1,25 @@
+const randomNum = 25; 
+
+function guessGame() {
+  let guess;
+
+  while (true) {
+    guess = parseInt(prompt("Please guess the number between 1 and 50:"));
+
+    if (isNaN(guess)) {
+      alert("Not a valid number. Please enter a valid number.");
+      continue;
+    }
+
+    if (guess === randomNum) {
+      alert(`Congratulations! You've guessed the number ${randomNumber} correctly.`);
+      break;
+    } else if (guess < randomNum) {
+      alert("Too low! Please, try again.");
+    } else {
+      alert("Too high! Please try again.");
+    }
+  }
+}
+
+guessGame();
